f89a3e65e4457c792e06fa753c904791a97ca4a2
Abdullah <graphicdesigner.abdullah@gmail.com>
2025-01-07 02:22:07 +0600

    Updated

--- index.js
+++ index.js
@@ -34,8 +34,8 @@ async function run() {
     // Collections
     const dataBase = client.db("BistroBossDB");
     const menuCollection = dataBase.collection("menu");
-    const rewardCollection = dataBase.collection("rewards");
-    const orderCollection = dataBase.collection("orders");
+    const reviewCollection = dataBase.collection("reviews");
+    const cartCollection = dataBase.collection("carts");
     const userCollection = dataBase.collection("users");
 
     // Get category counts for pagination of UI
@@ -60,6 +60,8 @@ async function run() {
       }
     });
 
+    // menu functionalities =======================================
+
     // get menu by filtered by category
     app.get("/menu", async (req, res) => {
       const { category } = req.query;
@@ -79,17 +81,48 @@ async function run() {
       }
     });
 
-    // get all rewards
-    app.get("/rewards", async (req, res) => {
-      const rewards = await rewardCollection.find().toArray();
-      res.json(rewards);
+    // carts functionalities =======================================
+
+    // get all carts filtered by single user
+    app.get("/carts", async (req, res) => {
+      const { query } = req.query;
+      // const carts = await cartCollection
+      //   .find({ customer_email: query })
+      //   .toArray();
+      const carts = await cartCollection
+        .aggregate([
+          { $match: { customer_email: query } },
+          {
+            $lookup: {
+              from: "menu",
+              localField: "menuId",
+              foreignField: "_id",
+              as: "menuDetails",
+            },
+          },
+          {
+            $unwind: "$menuDetails",
+          },
+          {
+            $project: {
+              _id: 1,
+              menuId: 1,
+              name: "$menuDetails.name",
+              image: "$menuDetails.image",
+              category: "$menuDetails.category",
+              price: "$menuDetails.price",
+            },
+          },
+        ])
+        .toArray();
+      res.send(carts);
     });
 
     // post a customer's order
-    app.post("/order-foods", async (req, res) => {
+    app.post("/carts", async (req, res) => {
       try {
         const food = req.body;
-        const result = await orderCollection.insertOne(food);
+        const result = await cartCollection.insertOne(food);
         res.status(200).send(result);
       } catch (error) {
         res.send({
@@ -97,6 +130,64 @@ async function run() {
           error: error,
         });
       }
+    });
+
+    // delete a single cart filtered by cart id
+    app.delete("/carts/:id", async (req, res) => {
+      const id = new ObjectId(req.params.id);
+      const result = await cartCollection.deleteOne({ _id: id });
+      res.send(result);
+    });
+
+    // rewards functionalities =======================================
+
+    // get all rewards
+    app.get("/reviews", async (req, res) => {
+      const rewards = await reviewCollection.find().toArray();
+      res.json(rewards);
+    });
+
+    // users functionalities =======================================
+
+    // get all users
+    app.get("/users", async (req, res) => {
+      const users = await userCollection.find().toArray();
+      res.send(users);
+    });
+
+    // get a single user id
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne(
+        { email: email },
+        { projection: { _id: 1 } }
+      );
+      res.status(200).send(user._id);
+    });
+
+    // create a single user
+    app.post("/users", async (req, res) => {
+      try {
+        const user = req.body;
+        const check = await userCollection.findOne({ email: user?.email });
+        if (!check) {
+          user.role = "customer";
+          const result = await userCollection.insertOne(user);
+          res.status(201).send(result);
+        } else res.send({ message: "User already exists" });
+      } catch (error) {
+        res.status(400).send({
+          message: "Error creating user",
+          error: error,
+        });
+      }
+    });
+
+    // delete a single user filtered by _id
+    app.delete("/users/:id", async (req, res) => {
+      const id = new ObjectId(req.params.id);
+      const result = await userCollection.deleteOne({ _id: id });
+      res.send(result);
     });
 
     // // Send a ping to confirm a successful connection
