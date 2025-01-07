77cd38d912b303ba0aaa73f2306108a6873be31b
Abdullah <graphicdesigner.abdullah@gmail.com>
2025-01-08 01:36:47 +0600

    Updated

--- index.js
+++ index.js
@@ -33,18 +33,6 @@ const verifyToken = (req, res, next) => {
   });
 };
 
-// verify admin after verify token
-const verifyAdmin = async (req, res, next) => {
-  const email = req.user.email;
-  const user = await userCollection.findOne({ email });
-  const isAdmin = user?.role === "admin";
-  if (!isAdmin)
-    return res
-      .status(403)
-      .send({ message: "Access denied. You are not an admin." });
-  next();
-};
-
 // const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rm6ii.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const uri = `mongodb://localhost:27017`;
@@ -73,6 +61,18 @@ async function run() {
       const token = jwt.sign(user, jwtSecret, { expiresIn: "23h" });
       res.send({ token });
     });
+
+    // verify admin after verify token
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.user.email;
+      const user = await userCollection.findOne({ email: email });
+      const isAdmin = user?.role === "admin";
+      if (!isAdmin)
+        return res
+          .status(403)
+          .send({ message: "Access denied. You are not an admin." });
+      next();
+    };
 
     // Get category counts for pagination of UI
     app.get("/menu/category-counts", async (req, res) => {
@@ -117,6 +117,13 @@ async function run() {
       }
     });
 
+    // post a menu item
+    app.post("/menu", verifyToken, verifyAdmin, async (req, res) => {
+      const menuItem = req.body;
+      const result = await menuCollection.insertOne(menuItem);
+      res.send(result);
+    });
+
     // carts functionalities =======================================
 
     // get all carts filtered by single user
@@ -155,7 +162,7 @@ async function run() {
     });
 
     // post a customer's order
-    app.post("/carts", async (req, res) => {
+    app.post("/carts", verifyToken, async (req, res) => {
       try {
         const food = req.body;
         const result = await cartCollection.insertOne(food);
@@ -169,7 +176,7 @@ async function run() {
     });
 
     // delete a single cart filtered by cart id
-    app.delete("/carts/:id", async (req, res) => {
+    app.delete("/carts/:id", verifyToken, async (req, res) => {
       const id = new ObjectId(req.params.id);
       const result = await cartCollection.deleteOne({ _id: id });
       res.send(result);
@@ -192,14 +199,14 @@ async function run() {
     });
 
     // get a single user id
-    app.get("/users/:email", async (req, res) => {
-      const email = req.params.email;
-      const user = await userCollection.findOne(
-        { email: email },
-        { projection: { _id: 1 } }
-      );
-      res.status(200).send(user._id);
-    });
+    // app.get("/users/:email", async (req, res) => {
+    //   const email = req.params.email;
+    //   const user = await userCollection.findOne(
+    //     { email: email },
+    //     { projection: { _id: 1 } }
+    //   );
+    //   res.status(200).send(user._id);
+    // });
 
     // check user is admin or customer
     app.get("/users/admin/:email", verifyToken, async (req, res) => {
@@ -233,14 +240,19 @@ async function run() {
     });
 
     // make admin to a single user by patch request
-    app.patch("/users/admin/:id", verifyToken, verifyAdmin, async (req, res) => {
-      const id = new ObjectId(req.params.id);
-      const result = await userCollection.updateOne(
-        { _id: id },
-        { $set: {role: "admin"} }
-      );
-      res.send(result);
-    });
+    app.patch(
+      "/users/admin/:id",
+      verifyToken,
+      verifyAdmin,
+      async (req, res) => {
+        const id = new ObjectId(req.params.id);
+        const result = await userCollection.updateOne(
+          { _id: id },
+          { $set: { role: "admin" } }
+        );
+        res.send(result);
+      }
+    );
 
     // delete a single user filtered by _id
     app.delete("/users/:id", verifyToken, verifyAdmin, async (req, res) => {
